394d643ff68d40d17489fc88c57d0eb3d26e260e
Megan Collier <meganraec@live.com>
2018-01-03 01:27:11 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,17 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"
+  ];
+  
+  function addElementToBeginningOfArray() {
+    
+  }
+  
+  function destructiveAddElementToBeginningOfArray() {
+    
+  }
+  
+  function 
+  
